c13c7c013c0d002645448dff64520d3d96333674
NEHRU22 <nehrugannamaneni@gmail.com>
2025-09-11 18:33:04 +0530

    Update Register.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -15,7 +15,7 @@ export default function Register() {
 
     try {
       // Change to your local backend URL when running locally
-      const res = await fetch("http://localhost:5000/api/auth/register"
+      const res = await fetch("https://maronadu-backend.onrender.com/api/auth/register"
 , {
         method: "POST",
         headers: {
